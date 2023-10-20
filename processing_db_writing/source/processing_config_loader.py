af0a64dfea2f83e577bd6cb9cc575f693cddc9cb
tarasqua <tarassel@mail.ru>
2023-10-20 14:22:58 +0300

    Забыл обработчик конфигов для кролика...

---
+++ processing_db_writing/source/processing_config_loader.py
@@ -0,0 +1,26 @@
+"""
+Load entrance detection config data
+"""
+
+import os
+import yaml
+from functools import reduce
+from pathlib import Path
+
+
+class ProcessingConfig:
+    """Processing config data loader"""
+
+    def __init__(self, config_file_name: str):
+        """
+        Parameters:
+            config_file_name: путь до config-файла
+        """
+        config_path = os.path.join(Path(__file__).resolve().parents[2], 'resources', 'config', config_file_name)
+        assert os.path.exists(config_path), "Config not found"
+        with open(config_path, 'r') as f:
+            self.config_loader: dict = yaml.load(f, Loader=yaml.FullLoader)
+
+    def get(self, *setting_name):
+        """Config getter"""
+        return reduce(dict.get, setting_name, self.config_loader)
